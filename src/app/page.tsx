e7ad0b204d17f60696e3ca3a0abfa4f86629b67a
Nar Shahin <narnarshahin@gmail.com>
2025-08-21 22:12:37 -0400

    image taking up half of home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,16 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="font-sans">
-      <main className="">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+      <main className="w-screen">
+        <div className="img-container w-screeb h-screen bg-blue relative">
+          <Image
+            src="/corner1.png"
+            alt="Akane Little"
+            layout="fill"
+            objectFit="cover"
+            priority
+          />
+        </div>
       </main>
     </div>
   );
