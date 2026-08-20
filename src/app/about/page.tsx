18455d68b4a196e03f2c489b2fa4f33548552983
medhatnar <narnarshahin@gmail.com>
2026-08-20 16:48:03 -0400

    test

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,7 +11,7 @@ export default function About() {
 
       <section className="two-col">
         <div className="portrait-frame">
-          <span>[Portrait Photo]</span>
+          <span>[Portro]</span>
         </div>
         <div>
           <h2>Hi, I&apos;m Akane</h2>
